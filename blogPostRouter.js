ebf3a21411a694b3855ca023259b601c90acf5ed
Peter Szujewski <pszujewski@gmail.com>
2017-03-16 09:57:22 -0400

    fixed problem with forEach now use for loops

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const uuid = require('uuid');
+
 const router = express.Router();
 const { BlogPosts } = require('./models'); 
 
@@ -18,43 +17,70 @@ router.get('/:id', (req, res) => {
 
 router.post('/', jsonParser, (req, res) => {
   const fields = ["title", "content", "author"];
-  fields.forEach(function(item) {
-    if(!req.body.hasOwnProperty(item)) {
-      let message = `Missing ${item}`;
+  for (let i=0; i<fields.length; i++) {
+    if(!req.body.hasOwnProperty(fields[i])) {
+      let message = `Missing ${fields[i]}`;
       console.error(message);
       return res.status(400).send(message);
     }
-  });
-  BlogPosts.create(req.body.title, req.body.content, req.body.author);
-  res.status(201).end();
+  }
+  let newPost = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(newPost);
 });
 
 router.delete('/:id', (req, res) => {
-  let message = `Post ID ${req.params.id} does not exist`;
-  BlogPosts.posts.forEach(function(object, i) {
-    if (req.params.id === object.id) {
+  for (let i=0; i<BlogPosts.posts.length; i++) {
+    let post = BlogPosts.posts[i]; 
+    if (req.params.id === post.id) {
       let message = `Post ID ${req.params.id} has been deleted`;
       BlogPosts.posts.splice(i, 1);
       return res.status(200).send(message);
     }
-  });
+  }
+  let message = `Post ID ${req.params.id} does not exist`;
   res.status(400).send(message);
 });
 
 router.put('/:id', jsonParser, (req, res) => {
-  BlogPosts.posts.forEach(function(item, index){
-    if (req.body.id === item.id) {
-      item.title = req.body.title;
-      item.author = req.body.author;
-      item.content = req.body.content;
-      let message = `blog item ${item.title} updated`;
-      return res.status(200).json(item);
+  for (let i=0; i<BlogPosts.posts.length; i++) {
+    let post = BlogPosts.posts[i];
+    if (req.params.id === post.id) {
+      post.title = req.body.title;
+      post.author = req.body.author;
+      post.content = req.body.content;
+      let message = `blog item ${post.title} updated`;
+      return res.status(200).json(post);
     }
-  });
-  let message = `Post ID ${req.params.id} does not exist`;
+  }
+  let message = `Put ID ${req.params.id} does not exist`;
   console.error(message);
   return res.status(400).send(message);
-});
+ }); 
+
+  // Old put follows --> not sure why but the forEach method is screwing up and running 
+  // both return statements 
+  // BlogPosts.posts.forEach(function(item, index){
+  //   if (req.body.id === item.id) {
+  //     item.title = req.body.title;
+  //     item.author = req.body.author;
+  //     item.content = req.body.content;
+  //     let message = `blog item ${item.title} updated`;
+  //     return res.status(200).json(item);
+  //   }
+  // });
+  // let message = `Post ID ${req.params.id} does not exist`;
+  // console.error(message);
+  // return res.status(400).send(message);
+
+  // Old post used forEach and the return statement/ res.send() in the forEach block failed to 
+  // End the .post function !
+  // fields.forEach(function(item) {
+  //   if(!req.body.hasOwnProperty(item)) {
+  //     let message = `Missing ${item}`;
+  //     console.error(message);
+  //     return res.status(400).send(message);
+  //   }
+  // });
 
 module.exports = { 
 	blogPostRouter: router, 
--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const uuid = require('uuid');
 const { BlogPosts } = require('./models'); 
-
 const { blogPostRouter, p1, p2 } = require('./blogPostRouter');
-
-const jsonParser = bodyParser.json(); 
 
 const app = express();
 
+app.use(morgan('common'));
 app.use('/blog-posts', blogPostRouter);
-
-// It should support the four CRUD operations for a blog posts resource.
-// GET and POST requests should go to /blog-posts.
-// DELETE and PUT requests should go to /blog-posts/:id.
-// Use Express router and modularize routes to /blog-posts.
-// Add a couple of blog posts on server load so you'll automatically have some data to look at when the server starts.
 
 app.listen(process.env.PORT || 8080, function () {
     console.log(`you're listening on ${process.env.PORT || 8080}`);
