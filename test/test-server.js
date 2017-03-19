5040b565f5ecc671def083141c0c76dd48ef3989
wshuang6 <seraph617@gmail.com>
2017-03-18 20:27:24 -0400

    working tests for get put post delete

--- test/test-server.js
+++ test/test-server.js
@@ -15,9 +15,52 @@ describe('blog-api-tests', function () {
     return chai.request(app)
       .get('/blog-posts')
       .then(function(res) {
-        // res.status.should.be(200);
+        res.should.has.status(200);
         res.should.be.json;
-        res.body.length.should.be.at.least(0);
+        res.body.length.should.be.at.least(1);
+        res.body.forEach((item) => {
+          item.should.include.keys(["id", "content", "title", "author", "publishDate"]);
+        });
+      })
+  });
+  it ('should create posts on POST', function () {
+    const testPost = {'title': 'thinkful', 'content': 'thinkfully', 'author': 'anonymous'}
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(testPost)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.include.keys(["id", "content", "title", "author", "publishDate"]);
+      })
+  })
+  it ('should update the first post on PUT', function () {
+    const putPost = {'title': 'thinkful', 'content': 'thinkfully', 'author': 'anonymous'}
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        const firstResultId = res.body[0].id;
+        putPost.id = firstResultId;
+        putPost.publishDate = res.body[0].publishDate;
+        return chai.request(app)
+          .put(`/blog-posts/${firstResultId}`)
+          .send(putPost)})
+      .then (function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.include.keys(["id", "content", "title", "author", "publishDate"]);
+        res.body.should.deep.equal(putPost);
+      });
+  });
+  it ('should delete the first item on DELETE', function () {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-posts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(200);
       })
   });
 });
